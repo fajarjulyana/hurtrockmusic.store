09d1df620ef1af95d6f448d23cc9f0f4331a3bf9
Fajar Julyana <fajarjulyana1@gmail.com>
2025-09-20 01:42:57 +0700

    deleted

--- client/src/components/examples/ContactSection.tsx
+++
@@ -1,9 +0,0 @@
-import ContactSection from '../ContactSection';
-
-export default function ContactSectionExample() {
-  return (
-    <ContactSection 
-      onFormSubmit={(data) => console.log('Contact form submitted:', data)}
-    />
-  );
-}
--- client/src/components/examples/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import Footer from '../Footer';
-
-export default function FooterExample() {
-  return (
-    <Footer 
-      onNewsletterSubmit={(email) => console.log('Newsletter signup:', email)}
-    />
-  );
-}

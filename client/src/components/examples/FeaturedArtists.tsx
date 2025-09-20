c976ef98d8d306d0928174ec1e3c3ac765c42eb2
Fajar Julyana <fajarjulyana1@gmail.com>
2025-09-20 14:20:48 +0700

    deleted

--- client/src/components/examples/FeaturedArtists.tsx
+++
@@ -1,9 +0,0 @@
-import FeaturedArtists from '../FeaturedArtists';
-
-export default function FeaturedArtistsExample() {
-  return (
-    <FeaturedArtists 
-      onArtistClick={(artistId) => console.log('Artist clicked:', artistId)}
-    />
-  );
-}

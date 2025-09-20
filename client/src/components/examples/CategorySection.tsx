ee8544d60077096e534cac828d3dce0f47a842d9
Fajar Julyana <fajarjulyana1@gmail.com>
2025-09-20 17:56:12 +0700

    deleted

--- client/src/components/examples/CategorySection.tsx
+++
@@ -1,9 +0,0 @@
-import CategorySection from '../CategorySection';
-
-export default function CategorySectionExample() {
-  return (
-    <CategorySection 
-      onCategoryClick={(categoryId) => console.log('Category clicked:', categoryId)}
-    />
-  );
-}
